59735f627c8574a4aa83bff3ca54b4e7b7738988
JYKil <kilga2402@gmail.com>
2021-12-20 23:05:05 +0900

    Update main.go

--- main.go
+++ main.go
@@ -2,6 +2,14 @@ package main
 
 import "fmt"
 
+func multiply(a,b int) int{
+  return a*b
+}
+
 func main() {
-fmt.Println("Hello world!")
+  // var name string = "kilga"
+  name := "kilga" // same syntax. It only works inside functions.  
+  name = "JY"
+  fmt.Println(name)
+  fmt.Println(multiply(2,2))
 }
